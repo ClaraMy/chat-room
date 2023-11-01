001fcb8dc3bfcaa5b29774459e64c3818ff17db3
xshuky <itsjuneforwork@gmail.com>
2023-11-01 16:51:12 +0100

    add function on click

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import s from "./Profile.module.scss";
 
 const Profile = () => {
+    // function to show up the controls when you click on your name profile
+    const [controlsVisible, setControlsVisible] = useState(false);
+
+    const visible = () => {
+        setControlsVisible(!controlsVisible);
+    };
+
     return (
         <div className={s.container}>
+            {controlsVisible &&
             <div className={s.controls}>
                 <p>do not disturb</p>
                 <p>log out</p>
             </div>
-            <div className={s.profile}>
+            }
+            <div className={s.profile} onClick={visible}>
                 <p>name</p>
             </div>
         </div>
