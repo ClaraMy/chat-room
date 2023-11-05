fd16cb343717d5c9a517065d5d8ef457515a6e64
xshuky <itsjuneforwork@gmail.com>
2023-11-05 21:33:48 +0100

    add function log out

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,16 +1,24 @@
+import { useRouter } from "next/router";
 import { useState } from "react";
 import s from "./Profile.module.scss";
 
 const Profile = () => {
     // function to show up the controls when you click on your name profile
     const [controlsVisible, setControlsVisible] = useState(false);
+    const {push} = useRouter();
 
     return (
         <div className={s.container}>
             {controlsVisible &&
             <div className={s.controls}>
                 <p>do not disturb</p>
-                <p>log out</p>
+                <p
+                    onClick={() => {
+                        localStorage.clear("username");
+                        localStorage.clear("sessionID");
+                        push ("/login");
+                    }}
+                >log out</p>
             </div>
             }
             <div 
